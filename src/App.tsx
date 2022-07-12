cb606303569760e97252039b1e9302375d553385
leo153624nid <barjastroy@gmail.com>
2022-07-12 21:28:45 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { useSelector } from 'react-redux'
 import './App.css'
 
 const App = () => {
+    const store = useSelector((store) => store)
+    console.log(store)
     return <div className="App">hello world</div>
 }
 
