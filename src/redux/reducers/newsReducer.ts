e221c7ce1cbee211ec52843bea033aad082419d7
leo153624nid <barjastroy@gmail.com>
2022-07-13 19:19:51 +0300

    Update newsReducer.ts

--- src/redux/reducers/newsReducer.ts
+++ src/redux/reducers/newsReducer.ts
@@ -2,23 +2,13 @@ import { createReducer } from '@reduxjs/toolkit'
 import { GET_POPULAR_NEWS } from '../constants'
 
 const initialState = {
-    userId: null,
-    login: null,
-    email: null,
-    userName: null,
-    userCars: null,
-    carusel: null,
+    news: null,
 }
 
 const newsReducer = createReducer(initialState, (builder) => {
     builder
         .addCase(GET_POPULAR_NEWS, (state, action) => ({
-            ...action.userData,
-            carusel: [
-                action.userData.userCars[0],
-                action.userData.userCars[1],
-                action.userData.userCars[2],
-            ],
+            state,
         }))
         .addDefaultCase((state) => state)
 })
