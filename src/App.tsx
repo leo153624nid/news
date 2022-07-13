4b942f95b76a219e29a98dfabd5de12283ac9e6c
leo153624nid <barjastroy@gmail.com>
2022-07-13 18:43:56 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { getPopularNews } from './redux/actions/actionCreator'
 import './App.css'
 
 const App = () => {
     const store = useSelector((store) => store)
+    const dispatch = useDispatch()
     console.log(store)
+
     return <div className="App">hello world</div>
 }
 
