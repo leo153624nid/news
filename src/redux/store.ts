ee7c70821f49c83868138f7b6052ee3c92c97813
leo153624nid <barjastroy@gmail.com>
2022-07-13 18:44:03 +0300

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,19 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './sagas'
 import rootReducer from './reducers/index'
 import logger from 'redux-logger'
 
+const sagaMiddleware = createSagaMiddleware()
+
 const store = configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({ thunk: false })
+            .concat(logger)
+            .concat(sagaMiddleware),
 })
+
+sagaMiddleware.run(rootSaga)
 
 export default store
