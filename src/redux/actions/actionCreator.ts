53d54e588bffe1065922ee3fccdc3098cc6dd00d
leo153624nid <barjastroy@gmail.com>
2022-07-14 20:37:21 +0300

    Update actionCreator.ts

--- src/redux/actions/actionCreator.ts
+++ src/redux/actions/actionCreator.ts
@@ -1,3 +1,21 @@
-import { GET_POPULAR_NEWS } from '../constants'
+import { GET_LATEST_NEWS, SET_LATEST_NEWS } from '../constants'
 
-export const getPopularNews = () => ({ type: GET_POPULAR_NEWS })
+type AType<T> = T extends { [key: string]: infer U } ? U : any
+export type ActionsType = ReturnType<AType<typeof actions>>
+
+export const actions = {
+    getLatestNews: () => ({ type: GET_LATEST_NEWS } as const),
+    setLatestNews: (payload: Object[]) =>
+        ({
+            type: SET_LATEST_NEWS,
+            payload,
+        } as const),
+}
+
+// export const getLatestNews = () => ({ type: GET_LATEST_NEWS } as const)
+
+// export const setLatestNews = (payload: Array<Object>) =>
+//     ({
+//         type: SET_LATEST_NEWS,
+//         payload,
+//     } as const)
