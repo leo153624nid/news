e8267caa9d8e9bbb0a235d2d1bbf8eed38c21d48
leo153624nid <barjastroy@gmail.com>
2022-07-14 21:39:38 +0300

    Update actionCreator.ts

--- src/redux/actions/actionCreator.ts
+++ src/redux/actions/actionCreator.ts
@@ -4,12 +4,12 @@ type InferValueTypes<T> = T extends { [key: string]: infer U } ? U : never
 export type ActionsType = ReturnType<InferValueTypes<typeof actions>>
 
 export const actions = {
-    getLatestNews: () => ({ type: GET_LATEST_NEWS } as const),
-    setLatestNews: (payload: any) =>
+    setLatestNews: (payload: Object[]) =>
         ({
             type: SET_LATEST_NEWS,
             payload,
         } as const),
+    getLatestNews: () => ({ type: GET_LATEST_NEWS } as const),
 }
 
 // export const getLatestNews = () => ({ type: GET_LATEST_NEWS } as const)
