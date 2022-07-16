dc3bbd0d4ff0ffe2dff7f5821b8bc2c8a11c73c8
leo153624nid <barjastroy@gmail.com>
2022-07-16 13:52:31 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import { useSelector, useDispatch } from 'react-redux'
 import React from 'react'
+import { useAppSelector, useAppDispatch } from './redux/hooks/hooks'
 import { actions } from './redux/actions/actionCreator'
 import './App.css'
 
 const App: React.FC = () => {
-    const store = useSelector((state) => state)
-    const dispatch = useDispatch()
+    const store = useAppSelector((state) => state)
+    const dispatch = useAppDispatch()
 
     const handleGetNews = () => {
         dispatch(actions.getLatestNews())
@@ -17,7 +17,7 @@ const App: React.FC = () => {
     return (
         <div className="App">
             <button type="button" onClick={handleGetNews}>
-                get news
+                Get News
             </button>
         </div>
     )
