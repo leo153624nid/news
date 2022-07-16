5e184317340d08b2ddc362d4ad9bbb638f30b112
leo153624nid <barjastroy@gmail.com>
2022-07-16 12:11:18 +0300

    Update newsAPI.ts

--- src/api/newsAPI.ts
+++ src/api/newsAPI.ts
@@ -1,3 +1,4 @@
+import { NewsType } from './../redux/reducers/newsReducer'
 import axios from 'axios'
 
 const axiosInstance = axios.create({
@@ -5,8 +6,8 @@ const axiosInstance = axios.create({
 })
 
 export const getLatestNews = async () => {
-    const request = await axiosInstance
+    const request = (await axiosInstance
         .get(`?query=react&tags=story&hitsPerPage=10&page=1`)
-        .then((response) => response.data)
+        .then((response) => response.data.hits)) as NewsType[]
     return await request
 }
