9f0dc28d05924b89158ac087650b33151ff12dc8
leo153624nid <barjastroy@gmail.com>
2022-07-16 12:11:20 +0300

    Update newsReducer.ts

--- src/redux/reducers/newsReducer.ts
+++ src/redux/reducers/newsReducer.ts
@@ -2,8 +2,22 @@ import { createReducer, PayloadAction } from '@reduxjs/toolkit'
 import { GET_LATEST_NEWS, SET_LATEST_NEWS } from '../constants'
 
 export type NewsType = {
-    id: string
-    story: string
+    author: string
+    comment_text: string | null
+    created_at: string
+    created_at_i: number
+    num_comments: number
+    objectID: string
+    parent_id: string | null
+    points: number
+    story_id: string | null
+    story_text: string | null
+    story_title: string | null
+    story_url: string | null
+    title: string
+    url: string
+    _highlightResult: object | null
+    _tags: object[] | null
 }
 type NewsState = { latestNews: NewsType[] }
 
