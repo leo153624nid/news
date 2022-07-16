d0451d2afb15e5b8d305577db706a869fc9e6080
leo153624nid <barjastroy@gmail.com>
2022-07-16 19:15:37 +0300

    CSS update

--- src/App.tsx
+++ src/App.tsx
@@ -10,53 +10,21 @@ import Home from './pages/Home/Home'
 import LatestNews from './pages/LatestNews/LatestNews'
 import PopularNews from './pages/PopularNews/PopularNews'
 
-const App: React.FC = () => (
+const App = () => (
     <>
         <Header />
+        <div className="main">
+            <Routes>
+                <Route path="/" element={<Home />} />
 
-        <Routes>
-            <Route path="/" element={<Home />} />
+                <Route path="/latest-news" element={<LatestNews />} />
 
-            <Route path="/latest-news" element={<LatestNews />} />
-
-            <Route path="/popular-news" element={<PopularNews />} />
-        </Routes>
+                <Route path="/popular-news" element={<PopularNews />} />
+            </Routes>
+        </div>
 
         <Footer />
     </>
 )
-
-// const App: React.FC = () => {
-//     const latestNews = useAppSelector((store) => store?.news?.latestNews || [])
-//     const popularNews = useAppSelector(
-//         (store) => store?.news?.popularNews || []
-//     )
-//     const { latestNewsError, popularNewsError } = useAppSelector(
-//         (store) => store?.errors || {}
-//     )
-//     const dispatch = useAppDispatch()
-
-//     const handleGetNews = () => {
-//         dispatch(actions.getNews())
-//     }
-
-//     return (
-//         <div className="App">
-//             <button type="button" onClick={handleGetNews}>
-//                 Get News
-//             </button>
-//             <News
-//                 news={latestNews}
-//                 label="Latest News"
-//                 error={latestNewsError}
-//             />
-//             <News
-//                 news={popularNews}
-//                 label="Popular News"
-//                 error={popularNewsError}
-//             />
-//         </div>
-//     )
-// }
 
 export default App
