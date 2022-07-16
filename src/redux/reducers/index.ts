9d2f6e360c2ef36bf5971a559982891168e17689
leo153624nid <barjastroy@gmail.com>
2022-07-16 15:13:11 +0300

    Update index.ts

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,8 +1,12 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import { combineReducers } from '@reduxjs/toolkit'
+import errorNewsReducer from './errorNewsReducer'
 import newsReducer from './newsReducer'
 
 const rootReducer = combineReducers({
     news: newsReducer,
+    errors: errorNewsReducer,
 })
 
 export default rootReducer
