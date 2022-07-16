f67a413701216dc2aaa7b7a63ca8a70bc7844d22
leo153624nid <barjastroy@gmail.com>
2022-07-16 13:52:29 +0300

    Update newsAPI.ts

--- src/api/newsAPI.ts
+++ src/api/newsAPI.ts
@@ -1,13 +1,13 @@
-import { NewsType } from './../redux/reducers/newsReducer'
+import { NewsType } from '../types/types'
 import axios from 'axios'
 
 const axiosInstance = axios.create({
     baseURL: 'http://hn.algolia.com/api/v1/search',
 })
 
-export const getLatestNews = async () => {
+export const getLatestNews = async (searchQuery: string) => {
     const request = (await axiosInstance
-        .get(`?query=react&tags=story&hitsPerPage=10&page=1`)
+        .get(`?query=${searchQuery}&tags=story&hitsPerPage=10&page=1`)
         .then((response) => response.data.hits)) as NewsType[]
     return await request
 }
