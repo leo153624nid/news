61cdb040e857e3d98ac35c72ae71a4431ae95bbd
leo153624nid <barjastroy@gmail.com>
2022-07-16 15:13:07 +0300

    Update constants.ts

--- src/redux/constants.ts
+++ src/redux/constants.ts
@@ -2,3 +2,5 @@
 export const GET_NEWS: string = 'GET_NEWS'
 export const SET_LATEST_NEWS: string = 'SET_LATEST_NEWS'
 export const SET_POPULAR_NEWS: string = 'SET_POPULAR_NEWS'
+export const SET_LATEST_NEWS_ERROR: string = 'SET_LATEST_NEWS_ERROR'
+export const SET_POPULAR_NEWS_ERROR: string = 'SET_POPULAR_NEWS_ERROR'
