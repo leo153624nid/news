8a3a7a4fff8ff512bf52416b604daa373da89512
leo153624nid <barjastroy@gmail.com>
2022-07-16 17:48:19 +0300

    init Routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,62 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 import React from 'react'
-import { useAppSelector, useAppDispatch } from './redux/hooks/hooks'
-import { actions } from './redux/actions/actionCreator'
+import { Route } from 'react-router-dom'
+// import { useAppSelector, useAppDispatch } from './redux/hooks/hooks'
+// import { actions } from './redux/actions/actionCreator'
 import './App.css'
-import News from './components/News/News'
+// import News from './components/News/News'
+import Home from './pages/Home/Home'
+import LatestNews from './pages/LatestNews/LatestNews'
+import PopularNews from './pages/PopularNews/PopularNews'
 
 const App: React.FC = () => {
-    const latestNews = useAppSelector((store) => store?.news?.latestNews || [])
-    const popularNews = useAppSelector(
-        (store) => store?.news?.popularNews || []
-    )
-    const { latestNewsError, popularNewsError } = useAppSelector(
-        (store) => store?.errors || {}
-    )
-    const dispatch = useAppDispatch()
-
-    const handleGetNews = () => {
-        dispatch(actions.getNews())
-    }
-
     return (
-        <div className="App">
-            <button type="button" onClick={handleGetNews}>
-                Get News
-            </button>
-            <News
-                news={latestNews}
-                label="Latest News"
-                error={latestNewsError}
-            />
-            <News
-                news={popularNews}
-                label="Popular News"
-                error={popularNewsError}
-            />
-        </div>
+        <>
+            <Header />
+
+            <Route path="/" element={<Home />} />
+
+            <Route path="/latest-news" element={<LatestNews />} />
+
+            <Route path="/popular-news" element={<PopularNews />} />
+
+            <Footer />
+        </>
     )
 }
+
+// const App: React.FC = () => {
+//     const latestNews = useAppSelector((store) => store?.news?.latestNews || [])
+//     const popularNews = useAppSelector(
+//         (store) => store?.news?.popularNews || []
+//     )
+//     const { latestNewsError, popularNewsError } = useAppSelector(
+//         (store) => store?.errors || {}
+//     )
+//     const dispatch = useAppDispatch()
+
+//     const handleGetNews = () => {
+//         dispatch(actions.getNews())
+//     }
+
+//     return (
+//         <div className="App">
+//             <button type="button" onClick={handleGetNews}>
+//                 Get News
+//             </button>
+//             <News
+//                 news={latestNews}
+//                 label="Latest News"
+//                 error={latestNewsError}
+//             />
+//             <News
+//                 news={popularNews}
+//                 label="Popular News"
+//                 error={popularNewsError}
+//             />
+//         </div>
+//     )
+// }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,23 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import { Router } from 'react-router'
+import { createBrowserHistory } from 'history'
 import store from './redux/store'
 import './index.css'
 import App from './App'
+
+const history = createBrowserHistory()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <Router history={history}>
+                <App />
+            </Router>
         </Provider>
     </React.StrictMode>
 )
