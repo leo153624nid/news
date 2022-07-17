8eb12219f9907cf9932d814935e389b6ab74febe
leo153624nid <barjastroy@gmail.com>
2022-07-17 11:56:38 +0300

    clean code

--- src/api/newsAPI.ts
+++ src/api/newsAPI.ts
@@ -1,5 +1,7 @@
-import { NewsType } from '../types/types'
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
 import axios from 'axios'
+import { NewsType } from '../types/types'
 
 const axiosInstance = axios.create({
     baseURL: 'http://hn.algolia.com/api/v1/search',
@@ -9,12 +11,12 @@ export const getNews = async (searchQuery: string) => {
     const request = (await axiosInstance
         .get(`?query=${searchQuery}&tags=story&hitsPerPage=10&page=1`)
         .then((response) => response.data.hits)) as NewsType[]
-    return await request
+    return request
 }
 
 export const getPopularNews = async () => {
     const request = (await axiosInstance
         .get(`?query=&tags=story&hitsPerPage=10&page=1`)
         .then((response) => response.data.hits)) as NewsType[]
-    return await request
+    return request
 }
